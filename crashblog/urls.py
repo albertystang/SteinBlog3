05ccfffbbce399681233d18c04f1aa9d7f5289ec
albertystang <99115311+albertystang@users.noreply.github.com>
2023-03-20 01:39:14 +0800

    Third commit

---
+++ crashblog/urls.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import sitemaps
+from django.contrib.sitemaps.views import sitemap
+from django.contrib import admin
+from django.urls import path, include
+
+from .sitemaps import CategorySitemap, PostSitemap
+
+from core.views import frontpage, about, robots_txt
+
+sitemaps = {'category': CategorySitemap, 'post': PostSitemap}
+
+urlpatterns = [
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}),
+    path('robots.txt', robots_txt, name='robots_txt'),
+    path('admin/', admin.site.urls),
+    path('about/', about, name='about'),
+    path('', include('blog.urls')),
+    path('', frontpage, name='frontpage'),
+] 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
